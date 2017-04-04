ce8563725edad7945eb154ed80ab8f6eddb8b7ad
Danil Simutin <danzilll@inbox.ru>
2017-04-05 02:10:54 +0700

    Refactored square equation solver code

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "solve.h"
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    solve(a, b, c);
+    return 0;
+}
---
+++ src/solve.c
@@ -0,0 +1,30 @@
+#include "solve.h"
+
+double discriminant(int a, int b, int c) {
+    return b*b - 4*a*c;
+}
+
+int solve(int a, int b, int c) {
+    double d = discriminant(a, b, c);
+    double x1, x2;
+    if (a == 0) {
+	printf("Not a square equation!");
+	return -1;
+    }
+    if (d > 0.0) {
+	x1 = (-b + sqrt(d))/(2*a);
+	x2 = (-b - sqrt(d))/(2*a);
+	printf("x1 = %f, x2 = %f", x1, x2);
+	return 2;
+    }
+    if (d == 0.0) {
+	x1 = -b/2*a;
+	printf("x = %f", x1);
+	return 1;
+    }
+    if (d < 0.0) {
+	printf("Only complex roots");
+	return 0;
+    }
+    return -10;
+}
